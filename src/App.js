d74a70854670abb4e0af05ffa74521983244cfa0
Xin Zheng <jason.zhengxin@gmail.com>
2019-10-28 14:41:03 +1100

    Create the sigin, signup page and button and input components

--- src/App.js
+++ src/App.js
@@ -3,36 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-const HatsPage = (props) => {
-  console.log(props);
-  return (< div >
-    <h1>Hats Page{props.match.params.id}</h1>
-  </div >)
-};
-const JacketsPage = (props) => {
-  console.log(props);
-  return (< div >
-    <h1>Jackets Page{props.match.params.id}</h1>
-  </div >)
-};
-
-const SneakersPage = (props) => {
-  console.log(props);
-  return (< div >
-    <h1>Sneaker Page{props.match.params.id}</h1>
-  </div >)
-};
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/jackets" component={JacketsPage} />
-        <Route exact path="/sneakers" component={SneakersPage} />
-        <Route path="/hats/:id" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
