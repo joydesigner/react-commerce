c2b37b048a8ca6106a507ab3abb183cc4d4c07ac
Xin Zheng <jason.zhengxin@gmail.com>
2019-10-30 22:51:36 +1100

    fix the div wrap issue for card-drop-down component and adjust minor style

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,10 +8,11 @@ import './cart-dropdown.styles.scss';
 
 const CartDropDown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
-    {
-      cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
-    }
+    <div className="cart-items" >
+      {
+        cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+      }
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
