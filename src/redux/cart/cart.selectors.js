5bf3198997d8d2cdb85080db85c3d5367938c194
Xin Zheng <jason.zhengxin@gmail.com>
2019-10-31 21:34:47 +1100

    add addItem and removeItem to checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,11 +4,12 @@ const selectCart = state => state.cart;
 // output selector
 export const selectCartItems = createSelector([selectCart], cart => cart.cartItems);
 
+export const selectCartHidden = createSelector([selectCart], cart => cart.hidden);
+
 export const selectCartItemsCount = createSelector([selectCartItems],
   cartItems =>
     cartItems.reduce((accumulatedQuantity, cartItem) =>
       accumulatedQuantity + cartItem.quantity, 0));
-export const selectCartHidden = createSelector([selectCart], cart => cart.hidden);
 
 export const selectCartTotal = createSelector([selectCartItems],
   cartItems =>
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,3 +1,5 @@
+import { Action } from "rxjs/internal/scheduler/Action";
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
     cartItem => cartItem.id === cartItemToAdd.id
@@ -8,4 +10,18 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem)
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem)
 }
