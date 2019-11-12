ed00b967cf0d28ba69b00c30ba7e3053f6ade506
Xin Zheng <jason.zhengxin@gmail.com>
2019-11-12 22:51:58 +1100

    move client app into client folder, added express server to help handle backend stripe payment

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_xlkO0Sc4GFB7x4uuNNAHmBI600k3H8RCzU';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('payment success')
+    }).catch(error => {
+      console.log('Payment error', JSON.parse(error));
+      alert('issue with the payment.');
+    })
+  }
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Jason's React ECommerce Platform"
+      billingAddress
+      shippingAddress
+      image="https://image.flaticon.com/icons/png/512/552/552788.png"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+
+export default StripeCheckoutButton;
